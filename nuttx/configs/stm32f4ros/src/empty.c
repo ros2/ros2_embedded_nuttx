fcada79c119970f3060aa788bf03e52c074363ed
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-08-26 11:21:59 -0700

    Strip down the stm32f4ros board config

---
+++ nuttx/configs/stm32f4ros/src/empty.c
@@ -0,0 +1,19 @@
+/*
+ * There are no source files here, but libboard.a can't be empty, so 
+ * we have this empty source file to keep it company.
+ */
+
+
+/************************************************************************************
+ * Name: stm32_boardinitialize
+ *
+ * Description:
+ *   All STM32 architectures must provide the following entry point.  This entry point
+ *   is called early in the intitialization -- after all memory has been configured
+ *   and mapped but before any devices have been initialized.
+ *
+ ************************************************************************************/
+void stm32_boardinitialize(void)
+{
+ 
+}
