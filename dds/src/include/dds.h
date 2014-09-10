cfabf14f6c81c6ddadcd308baca050d1b0e34657
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-09-10 13:37:05 -0700

    dds_notify fix the header to make sense with the impl.

--- dds/src/include/dds.h
+++ dds/src/include/dds.h
@@ -119,7 +119,8 @@ void dds_attach_notifier (unsigned nclass, DDSNOTFCT fct);
 
 /* Add a notifier function for notifications of the given class. */
 
-void dds_notify (unsigned nclass, Entity_t *ep, NotificationType_t type);
+void dds_notify (unsigned class, Entity_t *ep, unsigned status);
+//void dds_notify (unsigned nclass, Entity_t *ep, NotificationType_t type);
 
 /* Notify a listener for the given status. */
 
