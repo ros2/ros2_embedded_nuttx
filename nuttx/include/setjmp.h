120153f77c1e9c300327dab8aa16b675ad9d4795
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-09-22 16:28:48 -0700

    NuttX: add setjmp and longjmp
    Implementation based on klibc https://kernel.googlesource.com/pub/scm/libs/klibc/klibc/+/ac8f53aa182d6f423f1833e4bcc8d7d06adda03b/usr/klibc/arch/arm/setjmp.S

---
+++ nuttx/include/setjmp.h
@@ -0,0 +1,63 @@
+ /************************************************************************************
+ * include/setjmp.h
+ *
+ *   Copyright (C) 2014 Víctor Mayoral Vilches. All rights reserved.
+ *   Author: Víctor Mayoral Vilches <victor@osrfoundation.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ************************************************************************************/
+
+#ifndef _SETJMP_H
+#define _SETJMP_H
+
+/*
+ The jmp_buf is assumed to contain the following, in order:
+		r4
+		r5
+		r6
+		r7
+		r8
+		r9
+		r10
+		fp
+		sp
+		lr
+*/
+struct __jmp_buf {
+        unsigned int regs[10];
+};
+
+typedef struct __jmp_buf jmp_buf[1];
+
+
+__extern int setjmp(jmp_buf);
+__extern __noreturn longjmp(jmp_buf, int);
+
+
+#endif                          /* _SETJMP_H */
