f96bb03bcda77c9c101a9a4b87dc3f34a3b4c550
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-11-03 14:58:09 -0800

    Add ROS 2 Embedded applications folder

---
+++ apps/ros/publisher/main.c
@@ -0,0 +1,26 @@
+/*
+	ROS 2 Embedded Publisher Example
+*/
+
+#include "rcl.h"
+
+int ros_main(int argc, char *argv[])
+{
+	/* Init the ROS Client Library */
+	rcl_init();
+	
+	/* Create a ROS node */
+	create_node();
+
+	/* Create a publisher
+		TODO: specify message types, topics, etc.
+	*/
+	create_publisher();
+
+	int i;
+	for (i=0; i<10; i++){
+		publish("Hola ROS 2, ¿como estás?");
+	}
+
+}
+
