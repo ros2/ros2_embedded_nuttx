636d72484d0ffa4f63a66c85f3ecb83115ab6309
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-11-03 13:31:48 -0800

    Add a first prototype of RCL
    The code still needs to separate the dynamic types
    to an abstract interface. For now the ChatMsg and
    its functions are being used for testing purposes.

---
+++ rcl/rcl.h
@@ -0,0 +1,17 @@
+#ifndef __ros2_embedded__rcl__h__
+#define __ros2_embedded__rcl__h__
+
+void rcl_init(void);
+void create_node(void);
+void create_publisher(void);
+void publish(char* text_to_publish);
+
+void delete_publisher(void);
+void delete_node(void);
+
+/* TODO: ROS types */
+void init_types(void);
+void delete_types(void);
+
+
+#endif  // __ros2_embedded__rcl__h__
