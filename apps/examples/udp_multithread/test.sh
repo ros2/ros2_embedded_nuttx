dd4fccc909ae114a368848854241cdfeb2cb9b57
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-11-05 15:44:16 -0800

    Add tests to the UDP Multithread example

--- apps/examples/udp_multithread/test.sh
+++ apps/examples/udp_multithread/test.sh
@@ -2,8 +2,8 @@
 
 for i in $(seq 1 1 20)
 do
-	echo -n "foo 1" | nc -u -w1 192.168.0.3 6666
-	echo -n "foo 2" | nc -u -w1 192.168.0.3 6667
-	echo -n "foo 3" | nc -u -w1 192.168.0.3 6668
-	echo -n "foo 4" | nc -u -w1 192.168.0.3 6669
+	sudo sendip -p ipv4 -is 192.168.0.2 -p udp -us 5070 -ud 6666 -d "Hello 1" -v 192.168.0.3
+	sudo sendip -p ipv4 -is 192.168.0.2 -p udp -us 5071 -ud 6667 -d "Hello 2" -v 192.168.0.3
+	sudo sendip -p ipv4 -is 192.168.0.2 -p udp -us 5072 -ud 6668 -d "Hello 3" -v 192.168.0.3
+	sudo sendip -p ipv4 -is 192.168.0.2 -p udp -us 5073 -ud 6669 -d "Hello 4" -v 192.168.0.3
 done 
